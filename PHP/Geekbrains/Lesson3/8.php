f01c32f915ce4fee90b66d024f99181f23b0c338
JenerIIC <kgb85@mail.ru>
2021-03-21 11:11:43 +0300

    Add new solution method for task8 Lesson3PHP1HW

--- PHP/Geekbrains/Lesson3/8.php
+++ PHP/Geekbrains/Lesson3/8.php
@@ -1,9 +1,9 @@
 <?php
     // 8. *Повторить третье задание, но вывести на экран только города, начинающиеся с буквы «К».
     $regionAndCitys = [
-        'Московская область' => ['Зеленоград', 'Москва', 'Клин'],
+        'Московская область' => ['Зеленоград', 'Москва', 'Клин', 'Карповск'],
         'Ленинградская область' => ['Санкт-Петербург', 'Всеволжск', 'Павловск', 'Кронштадт'],
-        'Рязанская область' => ['Рязань', 'Искра', 'Листвянка'],
+        'Рязанская область' => ['Рязань', 'Искра', 'Листвянка', 'Кустья'],
     ];
 
     function onlyK($arr) {
@@ -11,11 +11,29 @@
             echo "$region: <br>";
             foreach($cites as $idx => $cityName) {
                 if(mb_substr($cityName, 0, 1) === "К") {
-                    echo "$cityName <br><br>";
+                    echo "$cityName,";
                 }
             }
+            echo "<br><br>";
         }
     }
-    
 
+    function onlyK0($arr) {
+        $str = "";
+        foreach($arr as $region => $cities) {
+            $str .= "$region: <br>";
+            foreach($cities as $idx => $cityCall) {
+                if(mb_substr($cityCall, 0, 1) === "К") {
+                    $str .= "$cityCall, ";
+                }
+            }
+            // $str = substr_replace($str, ".", strlen($str) - 2) . "<br><br>";
+            $str = rtrim($str, ", ");
+            $str .= ".<br><br>";
+        }   
+        echo $str;
+    }
+    
+    onlyK0($regionAndCitys);
+    echo "<hr>";
     onlyK($regionAndCitys);
