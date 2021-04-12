c84fde4fc522305931d83a4b4a8e0678f5e892ce
JenerIIC <kgb85@mail.ru>
2021-04-12 22:25:27 +0300

    Add new solution for Task1 Lesson5JS1HW

--- JavaScript/geekbrains/Lesson5/modalwindow/js/script.js
+++ JavaScript/geekbrains/Lesson5/modalwindow/js/script.js
@@ -1,34 +1,43 @@
 'use strict';
 
 let button = document.querySelector('button');
-let modalWindow = document.querySelector('.blackfog');
-let closeButton = modalWindow.querySelector('.close');
+let modalWindowBg = document.querySelector('.blackfog');
+let closeButton = modalWindowBg.querySelector('.close');
 let modal = document.querySelector('.modal');
 
-button.addEventListener('click', function(ev) {
-    let buttonActiv = ev.target;
-    buttonActiv.style.display = "none";
+button.addEventListener('click', function(evButton) {
+    evButton.target.style.display = "none";
 
-    modalWindow.classList.toggle("fade-in");
-    modalWindow.style.display = "flex";
-    modalWindow.addEventListener('animationend', () => {
+    let timeFunc = setTimeout(() => {modal.classList.toggle('flicker-in-1')}, 4000);
+
+    modalWindowBg.classList.toggle("fade-in");
+    modalWindowBg.style.display = "flex";
+    modalWindowBg.addEventListener('animationend', (evModalWindowBg) => {
+        evModalWindowBg.stopPropagation();
         modal.style.display="inline-block";
         modal.classList.toggle('roll-in-left');
-        modal.addEventListener('animationend', setTimeout(() => {
-            modal.classList.toggle('flicker-in-1');
-        }, 7000));
-        // modal.addEventListener('animationend', () => modal.classList.toggle('flicker-in-1'));
-        // modal.addEventListener('animationend', setTimeout(() => {
-        //     modal.classList.toggle('flicker-in-1'), 2000;
-        // }));
-    }); 
+        modal.addEventListener('animationend', (evModal) => {
+            timeFunc;
+            evModal.stopPropagation();
+        });
+    });
+    
+    setTimeout(() => {
+        modal.classList.toggle('roll-in-left');
+        modal.classList.toggle('flicker-in-1');
+        modalWindowBg.classList.toggle("fade-in");
+    }, 6000);
+
+    evButton.stopPropagation();
+
 });
 
-closeButton.addEventListener('click', function(event){
+closeButton.addEventListener('click', function(evCloseButton){
     modal.classList.toggle('flicker-in-1');
     setTimeout(() => {
         modal.style.display = "none", 2000;
     });
+    evCloseButton.stopPropagation();
     // modal.addEventListener('animationend', () => {modalWindow.classList.toggle('fade-off')});
     // setTimeout(() => modalWindow.classList.toggle('fade-off'), 3000);
     // setTimeout(() => modalWindow.style.display = "none", 3000);
