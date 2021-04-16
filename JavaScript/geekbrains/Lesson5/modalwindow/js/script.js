52a579a6c8bbeca98b30c02214bc6b84207b0765
JenerIIC <kgb85@mail.ru>
2021-04-16 23:08:32 +0300

    Task1 JS1Lesson5HW modal window

--- JavaScript/geekbrains/Lesson5/modalwindow/js/script.js
+++ JavaScript/geekbrains/Lesson5/modalwindow/js/script.js
@@ -4,43 +4,39 @@ let button = document.querySelector('button');
 let modalWindowBg = document.querySelector('.blackfog');
 let closeButton = modalWindowBg.querySelector('.close');
 let modal = document.querySelector('.modal');
+// let timeFunc = setTimeout(() => {modal.classList.toggle('flicker-in-1')}, 4000);
 
 button.addEventListener('click', function(evButton) {
     evButton.target.style.display = "none";
-
-    let timeFunc = setTimeout(() => {modal.classList.toggle('flicker-in-1')}, 4000);
-
-    modalWindowBg.classList.toggle("fade-in");
-    modalWindowBg.style.display = "flex";
-    modalWindowBg.addEventListener('animationend', (evModalWindowBg) => {
-        evModalWindowBg.stopPropagation();
-        modal.style.display="inline-block";
-        modal.classList.toggle('roll-in-left');
-        modal.addEventListener('animationend', (evModal) => {
-            timeFunc;
-            evModal.stopPropagation();
-        });
-    });
     
+    modalWindowBg.classList.add("fade-in");
+    modalWindowBg.style.display = "flex";
+
+    setTimeout(() =>{
+        modal.classList.add('roll-in-left');
+        modal.style.display="inline-block";
+    }, 2000);
+
     setTimeout(() => {
-        modal.classList.toggle('roll-in-left');
-        modal.classList.toggle('flicker-in-1');
-        modalWindowBg.classList.toggle("fade-in");
-    }, 6000);
+        modal.classList.remove('roll-in-left');
+        modalWindowBg.classList.remove("fade-in");
+    }, 4000);
 
     evButton.stopPropagation();
 
 });
 
-closeButton.addEventListener('click', function(evCloseButton){
-    modal.classList.toggle('flicker-in-1');
+closeButton.addEventListener('click', (evCloseButton) => {
+    modal.classList.toggle('roll-out-left');
+    modalWindowBg.classList.toggle('fade-out');
+
     setTimeout(() => {
-        modal.style.display = "none", 2000;
-    });
+        button.style.display = "inline-block";
+        modalWindowBg.style.display = "none";
+        modal.style.display="none";
+        modal.classList.toggle('roll-out-left');
+        modalWindowBg.classList.toggle('fade-out');
+    }, 1000);
+
     evCloseButton.stopPropagation();
-    // modal.addEventListener('animationend', () => {modalWindow.classList.toggle('fade-off')});
-    // setTimeout(() => modalWindow.classList.toggle('fade-off'), 3000);
-    // setTimeout(() => modalWindow.style.display = "none", 3000);
-    // setTimeout(() => button.style.display = "inline-block", 3000);
-    // setTimeout(() => modal.classList.toggle('flicker-in-1'), 3000);
 });
