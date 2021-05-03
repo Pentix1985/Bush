f6a77008dfd84ede0da43f6d3847281377624775
JenerIIC <kgb85@mail.ru>
2021-05-03 13:14:59 +0300

    VueJS Lessons 1,2

---
+++ VueJS/Lesson2/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const container = new Vue ({
+    el: '.container',
+    data: {
+        product: "Socks",
+        image: "vmSocks-blue.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+    },
+});
