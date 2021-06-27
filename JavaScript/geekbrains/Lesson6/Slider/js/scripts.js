68bbdd056d69c018ae5ae76a3cc36311e2f6c171
JenerIIC <kgb85@mail.ru>
2021-06-27 13:05:07 +0300

    Slider add styles

--- JavaScript/geekbrains/Lesson6/Slider/js/scripts.js
+++ JavaScript/geekbrains/Lesson6/Slider/js/scripts.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let img = document.querySelectorAll('img');
+
+let leftBtn = document.querySelector('.left-btn');
+let rightBtn = document.querySelector('.right-btn');
+
+leftBtn.addEventListener('click', (ev) => {
+    
+});
