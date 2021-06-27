d1495967458cf2aaa0169f84dd484764cb014fd4
JenerIIC <kgb85@mail.ru>
2021-06-28 01:33:56 +0300

    ez slider

--- JavaScript/geekbrains/Lesson6/Slider/js/scripts.js
+++ JavaScript/geekbrains/Lesson6/Slider/js/scripts.js
@@ -1,10 +1,47 @@
 'use strict';
 
-let img = document.querySelectorAll('img');
+let img = {
+    currentImg: 0,
+    images: [],
+
+    init() {
+        this.images = document.querySelectorAll('img');
+        this.showImg();
+    },
+
+    showImg() {
+        this.images[this.currentImg].classList.remove('hidden');
+    },
+
+    clickToLeft() {
+        this.images[this.currentImg].classList.add('hidden');
+        if(this.currentImg === 0) {
+            this.currentImg = this.images.length - 1;
+        } else {
+            this.currentImg--;
+        }
+        this.showImg();
+    },
+
+    clickToRight() {
+        this.images[this.currentImg].classList.add('hidden');
+        if(this.currentImg === this.images.length - 1) {
+            this.currentImg = 0;
+        } else {
+            this.currentImg++;
+        }
+        this.showImg();
+    },    
+}
 
 let leftBtn = document.querySelector('.left-btn');
-let rightBtn = document.querySelector('.right-btn');
+    leftBtn.addEventListener('click', (ev) => {
+        img.clickToLeft();
+    });
 
-leftBtn.addEventListener('click', (ev) => {
-    
-});
+let rightBtn = document.querySelector('.right-btn');
+    rightBtn.addEventListener('click', (ev) => {
+        img.clickToRight();
+    });
+
+img.init();
