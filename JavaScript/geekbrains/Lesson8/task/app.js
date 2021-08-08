fb329b361b73471c5623694693fef99aa9741a5a
JenerIIC <kgb85@mail.ru>
2021-08-08 14:28:43 +0300

    End Lesson8 main task

--- JavaScript/geekbrains/Lesson8/task/app.js
+++ JavaScript/geekbrains/Lesson8/task/app.js
@@ -30,26 +30,92 @@ filterSizeWrap.addEventListener('click', function() {
 });
 
 // header
-const cartIconCount = document.querySelector('.cartIconWrap>span');
 const cartIconButton = document.querySelector('.cartIconWrap');
 const basket = document.querySelector('.basket');
 
 // featured container
 const featuredImgDarkButton = document.querySelectorAll('.featuredImgDark button');
+const featuredItemsData = document.querySelectorAll('.featuredData');
 
 // Объект с функциями для работы с корзиной
 let funcObj = {
     /**
      * Функция назначается на все кнопки "Купить" в списке товаров
      */
-    onClickAddToCart() {
+    onClickAddToCart(event) {
+        let productInfo = event.currentTarget.parentNode.parentNode.parentNode.children[1];
         this.increaseCartIconCount();
+        this.addProductInBasket(productInfo);
+        this.setTotalCount();
+    },
+    /**
+     * Метод считает общую сумму
+     */
+    setTotalCount() {
+        const products = document.querySelectorAll('.basket__product');
+        const totalCount = document.querySelector('.basket__total-count');
+        let total = 0;
+        products.forEach(product => {
+            total += +product.querySelector('.basket__product-amount').innerText.substr(1);
+        });
+        totalCount.innerText = `$ ${total.toFixed(2)}`;
+    },
+
+    /**
+     * Функция добавляет товар в корзину
+     */
+    addProductInBasket(productInfo) {
+        let product = {};
+        product.name = productInfo.querySelector('.featuredName').innerText;
+        product.price = +productInfo.querySelector('.featuredPrice').innerText.substr(1);
+        // Проверяем есть ли товар в корзине
+        if(this.isProductInBasket(product)){
+            // Увеличиваем счетчик и сумму
+            let itemsInBasket = document.querySelectorAll('.basket__product');
+            for(let elem of itemsInBasket) {
+                if(elem.querySelector('.basket__product-name').innerText === product.name) {
+                    +elem.querySelector('.basket__product-count-number').innerText++;
+                    elem.querySelector('.basket__product-amount').innerText = `$ ${(+elem.querySelector('.basket__product-amount').innerText.substr(1) + +elem.querySelector('.basket__product-price').innerText.substr(1)).toFixed(2)}`;
+                }
+            }
+        } else {
+            // Вставляем карточку товара
+            document.querySelector('.basket__titles').insertAdjacentHTML('afterend', this.productItem(product));
+        };
+        
+    },
+    /**
+     * Проверяет наличие товара в корзине
+     * @param {boolean} product 
+     * @returns {boolean}
+     */
+    isProductInBasket(product) {
+        let itemsInBasket = document.querySelectorAll('.basket__product');
+        for(let elem of itemsInBasket) {
+            if(elem.querySelector('.basket__product-name').innerText === product.name) {
+                return true;
+            }
+        }
+        return false;
+    },
+
+    /**
+     * Функция возвращает карточку товара
+     */
+    productItem(product) {
+        return `<div class="basket__product">
+                <p class="basket__product-name">${product.name}</p>
+                <p class="basket__product-count"><span class="basket__product-count-number">1</span>шт.</p>
+                <p class="basket__product-price">$${product.price}</p>
+                <p class="basket__product-amount">$${product.price}</p>
+                </div>`
     },
 
     /**
      * Функция увеличивает счетчик при нажатии на кнопку "Купить"
      */
     increaseCartIconCount() {
+        const cartIconCount = document.querySelector('.cartIconWrap>span');
         +cartIconCount.innerText++;
     },
 
